629d5bb9894c4c880a419f718bbdce5640390984
ahardiag <ahardiag@gmail.com>
2023-06-06 14:28:34 +0200

    parse output data and create isotherms in CSV format

--- src/convert_data.py
+++ src/convert_data.py
@@ -1,7 +1,12 @@
 import os,glob
+from src.output_parser import *
+import pandas as pd
+import secrets
 
 data_dir = os.environ.get('DATA_DIR')
-sim_dir = os.environ.get(f'{data_dir}/simulations')
+sim_dir = f'{data_dir}/simulations'
+isotherm_dir =  f'{data_dir}/isotherms'
+os.makedirs(isotherm_dir,exist_ok=True)
 
 def check_simulations(verbose=False,
                       path=sim_dir):
@@ -9,7 +14,8 @@ def check_simulations(verbose=False,
     dir_many_outputs=[]
     warnings_one_output={}
     errors_one_output={}
-    for dir in os.listdir(path):
+    all_dirs = [ path for path in os.listdir(path) if os.path.isdir(path)]
+    for dir in all_dirs:
         l_data_files = glob.glob(f'{path}/{dir}/Output/System_0/*.data')
         if len(l_data_files)==1:
             filename = l_data_files[0]
@@ -22,8 +28,10 @@ def check_simulations(verbose=False,
 
     non_empty_warnings = {key:value for key,value in warnings_one_output.items() if isinstance(value, list) and len(value) > 0}
     non_empty_errors = {key:value for key,value in errors_one_output.items() if isinstance(value, list) and len(value) > 0}
-    print(f'{len(os.listdir(path))} directories found in {path}:')
+    print(f'{len(all_dirs)} directories found in {path}:')
     print(f"No       outputs found in {len(dir_no_outputs):5d} directories.")
+    if verbose is True : print(dir_no_outputs);print()
+    
     print(f"Multiple outputs found in {len(dir_many_outputs):5d} directories.")
     print(f"Warnings         found in {len(non_empty_warnings):5d} directories.")
     if verbose is True : print_dict(non_empty_warnings)
@@ -45,5 +53,38 @@ def print_dict(d):
             print(element)
         print()
 
-#def convert_to_csv(path=sim_dir):
+def output_isotherms_to_csv(input_path=sim_dir,output_path=isotherm_dir):
+    df = pd.read_csv(f'{sim_dir}/index.csv')
+    param_columns = df.columns.difference(['pressure', 'simkey']).to_list()
+    grouped = df.groupby(param_columns)
 
+    # Create an index file for isotherms
+    df_isot = pd.DataFrame()
+    for group, data in grouped:
+        simkeys = data["simkey"]
+        series_metadata_isot = data.iloc[0].drop('simkey')
+        series_metadata_isot["simkeys"] = simkeys.to_numpy()
+        series_metadata_isot["isokey"] = secrets.token_hex(6)
+        df_isot = df_isot.append(series_metadata_isot)
+    df_isot.to_csv(f'{isotherm_dir}/isotherms.csv',index=False)
+    print(f' File {isotherm_dir}/isotherms.csv have been created.\nAn isokey have been assigned to each isotherms.\n')
+    
+    # Create a CSV file for each isotherms
+    for index,row in df_isot.iterrows():
+        results =[]
+        simkeys = eval(row['simkeys'].replace(' ',','))
+        paths = [f'{data_dir}/simulations/{simkey}/Output/System_0/' for simkey in simkeys]
+        filenames = [os.listdir(path)[0] for path in paths]
+        for filename,path in zip(filenames,paths):
+            with open(os.path.join(path,filename),'r') as f:
+                string_output = f.read() 
+            results.append(parse(string_output))
+        gas = row['molecule_name']
+        uptakes = [[r['Thermo/Baro-stat NHC parameters']['External Pressure'][0],
+                    r["Number of molecules"][gas]
+                    ["Average loading absolute [cm^3 (STP)/cm^3 framework]"][0]]
+                    for r in results]
+        df_iso = pd.DataFrame(uptakes,columns=['pressure(bar)','uptake(cm^3 (STP)/cm^3 framework)']).sort_values(by='pressure(bar)')
+        df_iso['pressure(bar)'] = df_iso['pressure(bar)']/100000
+        file_out = f'{isotherm_dir}/{row["isokey"]}.csv'
+        df_iso.to_csv(file_out,index=False)
